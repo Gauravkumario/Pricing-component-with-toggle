b7b529454aec9dc543a3917672697430dee51d02
Gaurav Kumar <50291149+Gauravkumario@users.noreply.github.com>
2021-10-14 23:40:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,15 +14,16 @@ annuallybtn.addEventListener('click', () =>{
 function monthlyBtn() {
 	document.getElementById('annually-btn').style.visibility = "visible";
 	document.getElementById('monthly-btn').style.visibility = "hidden";
-	document.getElementById('value-1').textContent = "199.99";
-	document.getElementById('value-2').textContent = "249.99";
-	document.getElementById('value-3').textContent = "399.99";
+	document.getElementById('value-1').textContent = "19.99";
+	document.getElementById('value-2').textContent = "24.99";
+	document.getElementById('value-3').textContent = "39.99";
+	
 }
 /* ------------------------- */
 function annuallyBtn() {
 	document.getElementById('annually-btn').style.visibility = "hidden";
 	document.getElementById('monthly-btn').style.visibility = "visible";
-	document.getElementById('value-1').textContent = "19.99";
-	document.getElementById('value-2').textContent = "24.99";
-	document.getElementById('value-3').textContent = "39.99";
+	document.getElementById('value-1').textContent = "199.99";
+	document.getElementById('value-2').textContent = "249.99";
+	document.getElementById('value-3').textContent = "399.99";
 }
